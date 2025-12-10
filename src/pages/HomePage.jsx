22c02b21e10bd9f7651505c5a9cd707aa913caed
SABI9666 <sabincn676@gmail.com>
2025-12-10 11:15:27 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '../components/Navbar.jsx';
+import Footer from '../components/Footer.jsx';
 import { getProducts } from '../services/api';
 import './HomePage.css';
 
